f3518dbee9857151e0e3f2cc35fedc84037233b8
Daniel Victor Silva Bonfim <112987011+dasilvabonfim@users.noreply.github.com>
2024-01-12 11:04:32 -0400

    Add files via upload
    Site prontin

--- script.js
+++ script.js
@@ -1,16 +1,76 @@
+let musicas =[
+  {titulo:'Pitbull Cruel',artista:'Michaelo Jaquison',src:'musicas/pitbullcruel.mp3',img:'imagem/anya.jpg'},
+  {titulo:'Mc Poze anos 80',artista:'Não é o MC Poze',src:'musicas/mcpozeanos80.mp3',img:'imagem/superchoque.jpg'},
+  {titulo:'Mega meme remix',artista:'Caneta preta',src:'musicas/supermegamememix.mp3',img:'imagem/lulanaro.jpg'},
+];
+
 let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+
+renderizarMusica(indexMusica);
 
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
-function tocarMusica() {
-  musica.play();
-  document.querySelector('.botao-pause').style.display = 'block';
-  document.querySelector('.botao-play').style.display = 'none';
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 2;
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 2){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
 }
 
 function pausarMusica(){
     musica.pause();
     document.querySelector('.botao-pause').style.display = 'none';
     document.querySelector('.botao-play').style.display = 'block';
+}
+
+function atualizarBarra(){
+    let barra = document.querySelector('progress');
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
 }
